761b549596a9896121256405250811b98c315a4f
freccia <lol@lol.com>
2019-04-04 17:19:53 +0200

    update DES

--- src/ft_ssl_des.c
+++ src/ft_ssl_des.c
@@ -13,6 +13,8 @@
 #include "ft_ssl.h"
 #include "des.h"
 
+#include <sys/stat.h>
+
 int		ft_des(t_des *ctx, const int opt)
 {
 	(void)ctx;
@@ -26,5 +28,47 @@ int		ft_des(t_des *ctx, const int opt)
 	ft_printf("    ctx->salt: %s\n", ctx->salt);
 	ft_printf("    ctx->ivect: %s\n", ctx->ivect);
 	ft_printf("\n");
+
+	uint64_t	key;
+	uint64_t	ivect;
+	uint64_t	salt;
+	int			fdin;
+	int			fdout;
+	struct stat	infstat;
+	uint64_t	inlen;
+	int 		i;
+
+	i = 0;
+	if (ft_ishex(ctx->key) == EXIT_FAILURE)
+		ft_exit(EXIT_FAILURE, "Wrong key.\n");
+	if (ft_ishex(ctx->salt) == EXIT_FAILURE)
+		ft_exit(EXIT_FAILURE, "Wrong salt.\n");
+	if (ft_ishex(ctx->ivect) == EXIT_FAILURE)
+		ft_exit(EXIT_FAILURE, "Wrong ivect.\n");
+
+	key = ft_atoul_hex(ctx->key);
+	ivect = ft_atoul_hex(ctx->ivect);
+	salt = ft_atoul_hex(ctx->salt);
+
+	if ((fdin = open(ctx->infile, O_RDONLY)) < 0)
+		ft_exit(EXIT_FAILURE, "Failed to open file %s\n", ctx->infile);
+	if ((fdout = open(ctx->outfile, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR)) < 0)
+		ft_exit(EXIT_FAILURE, "Failed to open file %s\n", ctx->outfile);
+	if (fstat(int fdin, struct stat *infstat) < 0)
+		ft_exit(EXIT_FAILURE, "Failed fstat on file: %s\n", ctx->infile);
+
+	// PBKDF: SHA256
+
+	inlen = (uint64_t) infstat.st_size;
+	while (i < inlen)
+	{
+		// encrypt block
+
+		i =+ 64;
+	}
+	if (close(fdin) < 0)
+		ft_exit(EXIT_FAILURE, "Failed closing file: %s\n", ctx->infile);
+	if (close(fdout) < 0)
+		ft_exit(EXIT_FAILURE, "Failed closing file: %s\n", ctx->outfile);
 	return (EXIT_SUCCESS);
 }
